f16d9e672d33ed405f54cd30f91634adbe077aff
Yibo Yan <yiboyan@usc.edu>
2026-01-21 02:34:16 +0000

    add: more APIs

--- src/common/ddb_api.ts
+++ src/common/ddb_api.ts
@@ -5,6 +5,10 @@ const apiBaseUrl = process.env.DDB_API_URL || "http://localhost:5000";
 
 enum Endpoint {
   GetSessions = "/sessions",
+  ResolveSrcToGroupIds = "/src_to_grp_ids",
+  ResolveSrcToGroups = "/src_to_grps",
+  GetGroups = "/groups",
+  GetGroup = "/group",
   PendingCommands = "/pcommands",
   FinishedCommands = "/fcommands",
   Status = "/status",
@@ -28,6 +32,30 @@ export interface Session {
 
 export interface ServiceStatus {
   status: "up" | "down";
+}
+
+export interface GetGroupQuery {
+  grp_id?: number;
+  grp_hash?: string;
+}
+
+export interface GroupIdsResponse {
+  grp_ids: Set<number>;
+}
+
+export interface GroupsResponse {
+  grps: LogicalGroup[];
+}
+
+export interface SourceResolver {
+  src: string;
+}
+
+export interface LogicalGroup {
+  id: number;
+  hash: string;
+  alias: string;
+  sids: Set<number>;
 }
 
 export async function getSessions(): Promise<Session[]> {
@@ -69,4 +97,39 @@ export async function waitForServiceReady(
 
     await new Promise((resolve) => setTimeout(resolve, interval));
   }
+}
+
+export async function getGroups(): Promise<LogicalGroup[]> {
+  const response = await axios.get<LogicalGroup[]>(get_url(Endpoint.GetGroups));
+  return response.data;
+}
+
+export async function getGroup(query: GetGroupQuery): Promise<LogicalGroup> {
+  const response = await axios.get<LogicalGroup>(get_url(Endpoint.GetGroup), { params: query });
+  return {
+    ...response.data,
+    sids: new Set(response.data.sids),
+  }
+}
+
+/**
+ * GET /src_to_grp_ids?src=...
+ * Resolves a source string to a list of Group IDs
+ */
+export async function resolveSrcToGroupIds(src: string): Promise<Set<number>> {
+  const response = await axios.get<GroupIdsResponse>(get_url(Endpoint.ResolveSrcToGroupIds), {
+    params: { src } satisfies SourceResolver
+  });
+  return new Set(response.data.grp_ids);
+}
+
+/**
+ * GET /src_to_grps?src=...
+ * Resolves a source string to full GroupMeta objects
+ */
+export async function resolveSrcToGroups(src: string): Promise<LogicalGroup[]> {
+  const response = await axios.get<GroupsResponse>(get_url(Endpoint.ResolveSrcToGroups), {
+    params: { src } satisfies SourceResolver
+  });
+  return response.data.grps;
 }
